7c2114ab4e887e06675e92207810221984482984
Dileepa Samarawickrama <dnsam.mit@gmail.com>
2021-12-07 00:39:59 +0530

    create boilerplate files - init commit

---
+++ content.js
@@ -0,0 +1,6 @@
+(function () {
+  console.log("Chrome Extension Boilerplate (Manifest v2) by dnsamw");
+  console.log(
+    "https://github.com/dnsamw/-chrome-extension-boilerplate-manifest-v2.git"
+  );
+})();
